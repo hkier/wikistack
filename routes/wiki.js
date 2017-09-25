891cd1e3e4560d456bf8001da60ce36a5150cdb7
Howard Kier <howard@Howards-MacBook-Air.local>
2017-09-25 15:21:09 -0500

    review time

--- routes/wiki.js
+++ routes/wiki.js
@@ -19,7 +19,7 @@ router.get('/add', function (req, res, next) {
 router.get('/:urlTitle', function (req, res, next) {
     Page.findOne({ where: { urlTitle: req.params.urlTitle } })
         .then(function (found) {
-            res.render('../views/wikipage.html',{found: found})
+            res.render('../views/wikipage.html', { found: found })
         })
         .catch(function (err) {
             console.log(err);
@@ -29,17 +29,30 @@ router.get('/:urlTitle', function (req, res, next) {
 
 router.post('/', function (req, res, next) {
     // /wiki/post route
+    var user = User.findOrCreate({
+        where: {
+            name: req.body.author,
+            email: req.body.email
+        }
+    })
+        .then(function (vals) {
+            var user = vals[0];
 
-    var page = Page.build({
-        title: req.body.title,
-        content: req.body.content
-    });
+            var page = Page.build({
+                title: req.body.title,
+                content: req.body.content
+            });
 
-    page.save()
-        .then(obj => {
-            console.log(chalk.magenta(obj))
-            res.redirect('/wiki/' + obj.urlTitle)
+            page.save()
+                .then(page => {
+                    return page.setAuthor(user)
+                })
+                .then(obj => {
+                    console.log(chalk.magenta(obj))
+                    res.redirect('/wiki/' + obj.urlTitle)
+                })
         })
+        .catch(function (err) { console.log(err) })
 })
 
 
